6f070f39fc30e0fe2ce6feb801e03a94548353b4
santiagoae <santiago_avendano82162@elpoli.edu.co>
2023-08-12 10:08:12 -0500

    endpoint byId

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.company.inventory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,13 @@ public class CategoryRestController {
 	public ResponseEntity<CategoryResponseRest> searchCategories(){
 		
 		ResponseEntity<CategoryResponseRest> response = service.search();
+		return response;
+	}
+	
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable long id){
+		
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		return response;
 	}
 
